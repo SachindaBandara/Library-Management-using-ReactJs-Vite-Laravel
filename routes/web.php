093853aecdc5cfa265643909517198bab1ccf2f3
= <=>
2024-03-31 22:15:32 +0530

    nidi mathai

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/news', [NewsController::class, 'index'])->name('t');
 });
 
-Route::get('/news', [NewsController::class, 'index'])->name('t');
+
 
 require __DIR__.'/auth.php';
