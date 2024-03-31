36d5507e4552dc44b5ac2e6bdca8be3b8ab2ff47
= <=>
2024-03-31 16:06:13 +0530

    password reset eka hari

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -23,17 +23,20 @@
         </div>
     </div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="card">
+                <div class="card-body">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
+        </div>
+    </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="card">
+                <div class="card-body">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,83 +1,24 @@
-<section>
-    <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Update Password') }}
-        </h2>
-
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
-        </p>
-    </header>
-
-    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
-        @csrf
-        @method('put')
-
-        <div>
-            <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-            <x-text-input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
-            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
-        </div>
-
-        <div>
-            <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
-        </div>
-
-        <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-            @if (session('status') === 'password-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
-            @endif
-        </div>
-    </form>
-</section>
-
-<h5 class="card-title">Profile Information</h5>
-<p>Update your account's profile information and email address.</p>
-<form id="send-verification" method="POST" action="{{ route('verification.send') }}">
+<h5 class="card-title">Update Password</h5>
+<p>Ensure your account is using a long, random password to stay secure.</p>
+<form method="POST" action="{{ route('password.update') }}" class="row g-3 needs-validation">
     @csrf
-</form>
-<form method="POST" action="{{ route('profile.update') }}" class="row g-3 needs-validation">
-    @csrf
-    @method('patch')
+    @method('put')
     <div class="col-12">
-        <label for="name" class="form-label">Name</label>
-        <input type="text" name="name" class="form-control" id="name" value="{{ old('name', $user->name) }}" required autofocus autocomplete="name"/>
-        <div class="invalid-feedback">{{ "$errors->get('name')" }}</div>
+        <label for="update_password_current_password" class="form-label">Current Password</label>
+        <input type="password" name="current_password" class="form-control" id="update_password_current_password" required  autocomplete="current-password"/>
+        <div class="invalid-feedback">{{ "$errors->updatePassword->get('current_password')" }}</div>
     </div>
 
     <div class="col-12">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" name="email" class="form-control" id="email" value="{{ old('email', $user->email) }}" required autofocus autocomplete="username"/>
-        <div class="invalid-feedback">{{ "$errors->get('email')" }}</div>
+        <label for="update_password_password" class="form-label">New Password</label>
+        <input type="password" name="password" class="form-control" id="update_password_current_password" required  autocomplete="new-password"/>
+        <div class="invalid-feedback">{{ "$errors->updatePassword->get('password')" }}</div>
+    </div>
 
-        @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-            <div>
-                <p>Your email address is unverified.<button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Click here to re-send the verification email.</button></p>
-                @if (session('status') === 'verification-link-sent')
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        <i class="bi bi-check-circle"></i>
-                        A new verification link has been sent to your email address.
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif
-            </div>
-        @endif
+    <div class="col-12">
+        <label for="update_password_password_confirmation" class="form-label">Confirm Password</label>
+        <input type="password" name="password_confirmation" class="form-control" id="update_password_password_confirmation" required  autocomplete="new-password"/>
+        <div class="invalid-feedback">{{ "$errors->updatePassword->get('password_confirmation')" }}</div>
     </div>
 
     <div class="col-12">
@@ -85,10 +26,10 @@
     </div>
 
     <div class="col-12">
-        @if (session('status') === 'profile-updated')
+        @if (session('status') === 'password-updated')
             <div class="alert alert-success alert-dismissible fade show" role="alert">
                 <i class="bi bi-check-circle"></i>
-                Profile Updated
+                Password Saved !
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
