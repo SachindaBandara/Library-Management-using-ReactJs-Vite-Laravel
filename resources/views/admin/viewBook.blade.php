41d70b090ad1fef5d5fca374936cbc5095f14aaa
= <=>
2024-04-18 19:00:21 +0530

    issued history eka hadanna gaththa

--- resources/views/admin/viewBook.blade.php
+++ resources/views/admin/viewBook.blade.php
@@ -81,6 +81,39 @@
             </div>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-xl-8">
+
+            <div class="card">
+                <div class="card-body pt-3">
+                    <!-- Bordered Tabs -->
+                    <ul class="nav nav-tabs nav-tabs-bordered">
+                        <li class="nav-item">
+                            <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#issuedHistory">Issued History</button>
+                        </li>
+
+                        <li class="nav-item">
+                            <button class="nav-link" data-bs-toggle="tab" data-bs-target="#receivedHistory">Received History</button>
+                        </li>
+                    </ul>
+                    <div class="tab-content pt-2">
+
+                        <div class="tab-pane fade show active" id="issuedHistory">
+
+                        </div>
+
+                        <div class="tab-pane fade profile-edit pt-3" id="receivedHistory">
+                        <!-- Profile Edit Form -->
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </section>
 
 @endsection
