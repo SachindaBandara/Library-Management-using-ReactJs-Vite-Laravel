f022457b3fed06888c7615acae1bc5b33e4f1502
= <pinildissanayaka@gmail.com>
2024-06-22 14:22:02 +0530

    working on reservation

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Reservation;
 use App\Models\Book;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ReservationController extends Controller
 {
     //
     public function reservationsUser(){
-        $reservations=Reservation::all();
+        $member_id=Auth::user()->id;
+
+        $reservations=Reservation::where('member_id', $member_id)->get();
+
         return view('user.reservation', ['reservations' => $reservations]);
     }
 
