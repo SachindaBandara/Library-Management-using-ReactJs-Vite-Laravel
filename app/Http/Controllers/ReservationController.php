aca6deea2f625a03eff92870d36004fc76f1a08c
= <pinildissanayaka@gmail.com>
2024-06-22 14:14:31 +0530

    working on reservation table

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Reservation;
+use App\Models\Book;
 
 class ReservationController extends Controller
 {
     //
     public function reservationsUser(){
-        return view('user.reservation');
+        $reservations=Reservation::all();
+        return view('user.reservation', ['reservations' => $reservations]);
     }
 
 
--- resources/views/user/reservation.blade.php
+++ resources/views/user/reservation.blade.php
@@ -26,7 +26,7 @@
 
 <section class="section">
     <div class="row">
-        <!-- books -->
+        <!-- Reservations -->
         <div class="col-12">
             <div class="card add-book overflow-auto">
                 <div class="card-body">
@@ -36,44 +36,36 @@
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
-                                <th scope="col">Title</th>
-                                <th scope="col">ISBN</th>
-                                <th scope="col">Author</th>
-                                <th scope="col">Genre</th>
-                                <th scope="col">Publication Year</th>
-                                <th scope="col">Description</th>
+                                <th scope="col">Book Title</th>
                                 <th scope="col">Shelf Location</th>
-                                <th scope="col"> </th>
+                                <th scope="col">Reserved date</th>
+                                <th scope="col">Status</th>
                             </tr>
                         </thead>
                         <tbody>
-
+                            @foreach ($reservations as $reservation)
+                                <tr>
+                                    <th scope="row"><a href="#">{{ $reservation -> id}}</a></th>
+                                    <td>{{ $reservation -> title}}</td>
+                                    <td>{{ $reservation -> shelfLocation}}</td>
+                                    <td>{{ $reservation -> reserved_date}}</td>
+                                    <td>
+                                        @if (($reservation -> status) == 'Available')
+                                            <span class="badge rounded-pill bg-success">Available</span>
+                                        @elseif (($reservation -> status) == 'Borrowed')
+                                            <span class="badge rounded-pill bg-secondary">Borrowed</span>
+                                        @elseif (($reservation -> status) == 'Reserved')
+                                            <span class="badge rounded-pill bg-warning text-dark">Reserved</span>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
-        <!-- End Book  -->
-    </div>
-
-    <div class="col-12">
-        @if ($errors -> any())
-            @foreach ($errors->all() as $error)
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    <i class="bi bi-exclamation-circle"></i>
-                    {{$error}}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            @endforeach
-        @endif
-
-        @if (session('success'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <i class="bi bi-check-circle"></i>
-                {{ session('success') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        @endif
+        <!-- End Reservations  -->
     </div>
 </section>
 
