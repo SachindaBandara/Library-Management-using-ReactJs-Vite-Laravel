f17efc724e914e37cacbeae5b72cd242ff7a5a38
= <pinildissanayaka@gmail.com>
2024-06-23 00:10:40 +0530

    finish reset password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,68 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+@extends('auth.layout')
+@section('main')
+    <div class="container">
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+        <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+                        <div class="d-flex justify-content-center py-4">
+                            <a href="/" class="logo d-flex align-items-center w-auto">
+                                <img src="assets/img/logo.jpg" alt="">
+                            </a>
+                        </div><!-- End Logo -->
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                        <div class="card mb-3">
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                            <div class="card-body">
+                                <form class="row g-3 needs-validation" novalidate method="POST" action="{{ route('password.store') }}">
+                                    @csrf
+                                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
+                                    <div class="col-12">
+                                        <label for="email" class="form-label" >Email :</label>
+                                        <div class="input-group has-validation">
+                                            <input type="email" name="email" class="form-control" id="email" placeholder="Enter email" :value="old('email', $request->email)" required autofocus autocomplete="username">
+                                            <div class="invalid-feedback">
+                                                @foreach ($errors->get('email') as $error)
+                                                    {{ $error }}
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                                    <div class="col-12">
+                                        <label for="password" class="form-label">Password</label>
+                                        <input type="password" name="password" class="form-control" placeholder="Password" id="password" required autocomplete="new-password">
+                                        <div class="invalid-feedback">
+                                            @foreach ($errors->get('password') as $error)
+                                                {{ $error }}
+                                            @endforeach
+                                        </div>
+                                    </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+                                    <div class="col-12">
+                                        <label for="password_confirmation" class="form-label">Password</label>
+                                        <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm Password" id="password_confirmation" required autocomplete="new-password">
+                                        <div class="invalid-feedback">
+                                            @foreach ($errors->get('password_confirmation') as $error)
+                                                {{ $error }}
+                                            @endforeach
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <button class="btn btn-primary w-100" type="submit">Reset Password</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </div>
+@endsection
+
