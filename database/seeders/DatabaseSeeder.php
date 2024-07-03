3689a56dea3d1760e7ac0a165cdc4c4637552596
= <pinildissanayaka@gmail.com>
2024-07-03 21:30:08 +0530

    create book seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,6 +19,12 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('12345678'),
+            'userType' => 'admin'
+        ]);
+
+        $this->call([
+            BookSeeder::class
         ]);
     }
 }
