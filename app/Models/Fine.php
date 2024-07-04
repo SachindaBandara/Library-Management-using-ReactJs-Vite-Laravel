7152a2e9181ec642dd3c327c8c323ee4944cd9eb
= <pinildissanayaka@gmail.com>
2024-07-04 17:53:21 +0530

    add fine model

--- app/Models/Fine.php
+++ app/Models/Fine.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fine extends Model
 {
+    protected $fillable = [
+        'member_id',
+        'transaction_id',
+        'amount',
+        'paid_at'
+    ];
     use HasFactory;
 }
