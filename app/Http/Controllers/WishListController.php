07509d489869a4edc729b590e133fc89b0d56b13
= <pinildissanayaka@gmail.com>
2024-07-05 18:57:38 +0530

    make wishlist controller

---
+++ app/Http/Controllers/WishListController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\WishList;
+
+class WishListController extends Controller
+{
+    //
+    public function addWishlistUser(){
+
+    }
+}
