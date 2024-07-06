3d75437dde59b7e765c5a77fd5819ef54303f951
= <pinildissanayaka@gmail.com>
2024-07-06 07:08:27 +0530

    change wishlist delete end

--- resources/views/layouts/indexLayout.blade.php
+++ resources/views/layouts/indexLayout.blade.php
@@ -123,7 +123,7 @@
         </div>
 
         <div class="container copyright text-center mt-4">
-            <p>© <span>Copyright</span> <strong class="px-1">SiteName</strong> <span>All Rights Reserved</span></p>
+            <p>© <span>Copyright</span> <strong class="px-1">ShelfSpace</strong> <span>All Rights Reserved</span></p>
             <div class="credits">
                 Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
             </div>
